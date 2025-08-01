a976db65c196a349a07178a134d80a9fe8396ff1
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2025-08-01 22:06:21 +1000

    Add more languages

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,16 +1,31 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "autopep8" },
-				rust = { "rustfmt", lsp_format = "fallback" },
-				javascript = { "prettierd" },
-				c = { "clang-format" },
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "isort", "autopep8" },
+                rust = { "rustfmt", lsp_format = "fallback" },
+                javascript = { "prettierd", "prettier" },
+                typescript = { "prettierd", "prettier" },
+                jsx = { "prettierd", "prettier" },
+                c = { "clang-format" },
                 json = { "prettierd", "prettier" },
-			},
-		})
-	end,
+                tex = { "latexindent" },
+                html = { "prettierd", "prettier" },
+                css = { "prettierd", "prettier" }
+            },
+
+            formatters = {
+                latexindent = {
+                    command = "latexindent",
+                    args = {
+                        "-m",
+                    },
+                    stdin = false,
+                },
+            },
+        })
+    end,
 }
